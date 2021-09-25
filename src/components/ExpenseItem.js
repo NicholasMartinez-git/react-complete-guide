37da0e369fec87679a55b838737e6761d2edf384
Nicholas Martinez <78042630+NicholasMartinez-git@users.noreply.github.com>
2021-09-25 09:19:25 -0500

    feat: Update components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,19 +1,21 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
+function ExpenseItem(props) {
 
-    // illustrating dummy dynamic code form looking at dummy data.
-    const expenseDate = new Date(2021, 9, 23);
-    const expenseTitle = "Car Insurance";
-    const expenseAmount = 294.67; 
-
+    const month = props.date.toLocaleString('en-US', {month: 'long'});
+    const day = props.date.toLocaleString('en-US', {day: '2-digit'});
+    const year = props.date.getFullYear();
 
     return (
         <div className="expense-item">
-            <div>{expenseDate.toDateString()}</div>
+            <div>
+                <div>{month}</div>
+                <div>{day}</div>
+                <div>{year}</div>
+            </div>
             <div className="expense-item__description">
-                <h2>{expenseTitle}</h2>
-                <div className="expense-item__price">${expenseAmount}</div>
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">${props.amount}</div>
             </div>
         </div>
     );
